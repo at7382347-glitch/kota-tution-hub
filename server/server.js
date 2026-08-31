c60d78b0a20e8702649819faba60c83cdfbf7bfe
salar <salar04409@gmail.com>
2026-09-01 02:07:37 +0530

    Simplify CORS configuration to fix preflight handling

--- server/server.js
+++ server/server.js
@@ -24,13 +24,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+  origin: allowedOrigins,
+  credentials: true
 }));
 app.use(express.json());
 app.use('/uploads', express.static(uploadsDir));
